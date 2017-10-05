ccf21c31d77dced381ca554ea0e5db4d4dc18bfe
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-05 11:54:23 +0200

    Update login.php

--- login.php
+++ login.php
@@ -8,43 +8,46 @@
 @ $db = new mysqli('localhost', 'root', '', 'juarvis');
 session_start();
 
-if ($db->connect_error) {
-    echo "could not connect: " . $db->connect_error;
-    printf("<br>login failed");
-    exit();
-}
+$error=''; // Variable To Store Error Message
 
-    #the mysqli_real_espace_string function helps us solve the SQL Injection
-    #it adds forward-slashes in front of chars that we can't store in the username/pass
-    #in order to excecute a SQL Injection you need to use a ' (apostrophe)
-    #Basically we want to output something like \' in front, so it is ignored by code and processed as text
-
-if (isset($_POST['username'], $_POST['userpass'])) {
-    #SQL Injection-proof
-    $uname = htmlentities($_POST['username']);
-    $uname = mysqli_real_escape_string($db, $_POST['username']);
-    
-    #here we hash the password, and we want to have it hashed in the database as well
-    #optimally when you create a user (through code) you simply send a hash
-    #hasing can be done using different methods, MD5, SHA1 etc.
-    
-    $upass = sha1($_POST['userpass']);
-    
-    $query = ("SELECT * FROM users WHERE username = '{$uname}' "." AND hash = '{$upass}'");
-       
-    
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    $stmt->store_result(); 
-    
-    #here we create a new variable 'totalcount' just to check if there's at least
-    #one user with the right combination. If there is, we later on print out "access granted"
-    $totalcount = $stmt->num_rows();
-    
-    
-    
+if (isset($_POST['submit'])) {
+	
+if (empty($_POST['username']) || empty($_POST['password'])) {
+	$error = "Username or Password is invalid";
 }
-?>
+else
+{
+	// Define $username and $password
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	
+	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+	$connection = mysql_connect("localhost", "root", "juarvis");
+	
+	// To protect MySQL injection for Security purpose
+	$username = stripslashes($username);
+	$username = htmlentities($username);
+	$password = stripslashes($password);
+	$username = mysql_real_escape_string($username);
+	$password = mysql_real_escape_string($password);
+	
+	// Selecting Database
+	$db = mysql_select_db("users", $connection);
+	
+	// SQL query to fetch information of registerd users and finds user match.
+	$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
+	$rows = mysql_num_rows($query);
+	
+if ($rows == 1) {
+	$_SESSION['username']=$username; // Initializing Session
+	header("location: main.php"); // Redirecting To home page
+} else {
+	$error = "Username or Password is invalid";
+	}
+	mysql_close($connection); // Closing Connection
+	}
+}
+?> 
 
 <div class="login-form">
 			<div class="top-login">
