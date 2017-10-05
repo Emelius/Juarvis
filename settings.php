307fe21b4397bd74995ba5d9cb328981c6f1ef71
Hanna <hanna_96eri@hotmail.com>
2017-10-05 11:04:55 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,1 +1,8 @@
-// change pw, username, colorscheme
+<?php
+include 'header.php';
+?>
+/*/table listing following: to change username, name, change pw, email, colorscheme?
+change button - ability to change - send new info to db /*/
+<?php
+include 'footer.php';
+?>
