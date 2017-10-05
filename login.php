cbcfd2e5078739185eedcfc49afad790628be149
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-05 13:08:08 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,39 @@
+
+
+//@ $db = new mysqli('localhost', 'root', '', 'juarvis');
+//session_start();
+ 
+
+
 <?php
-
-@ $db = new mysqli('localhost', 'root', '', 'juarvis');
-session_start();
-
-?> 
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: main.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
 
 <div class="login-form">
 			<div class="top-login">
@@ -14,12 +44,12 @@ session_start();
 			<form>
 				<div class="login-ic">
 					<i ></i>
-					<input type="text"  value="User name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User name';}"/>
+					<input type="text"  value="username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User name';}"/>
 					<div class="clear"> </div>
 				</div>
 				<div class="login-ic">
 					<i class="icon"></i>
-					<input type="password"  value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}"/>
+					<input type="password"  value="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}"/>
 					<div class="clear"> </div>
 				</div>
 			
