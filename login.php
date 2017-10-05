d299791ef918deb23069fd73494c13a1aed43d0f
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-05 12:15:56 +0200

    Update login.php

--- login.php
+++ login.php
@@ -7,14 +7,14 @@ $error=''; // Variable To Store Error Message
 
 if (isset($_POST['submit'])) {
 	
-if (empty($_POST['username']) || empty($_POST['password'])) {
+if (empty($_POST['username']) || empty($_POST['userpass'])) {
 	$error = "Username or Password is invalid";
 }
 else
 {
 	// Define $username and $password
 	$username=$_POST['username'];
-	$password=$_POST['password'];
+	$password=$_POST['userpass'];
 	
 	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
 	$connection = mysql_connect("localhost", "root", "juarvis");
