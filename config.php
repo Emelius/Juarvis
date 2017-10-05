c2d4c8809ab8707c56116784ec094c9c3d454ef0
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-05 13:02:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,22 @@
+//<?php
+//$url = $_SERVER['REQUEST_URI'];
+
+//$strings = explode('/', $url);
+
+//$dbname = 'juarvis';
+//$dbuser = 'root';
+//$dbpass = '';
+//$dbserver = 'localhost';
+
+
+//?>
+
 <?php
-$url = $_SERVER['REQUEST_URI'];
 
-$strings = explode('/', $url);
-
-$dbname = 'juarvis';
+$dbserver = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
-$dbserver = 'localhost';
+$dbname = 'juarvis';
 
-
+   $db = mysqli_connect($dbserver,$dbuser,$dbpass,$dbname);
 ?>
