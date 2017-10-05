604fd564cac4df848e04ab85d789499642f51104
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-05 12:34:53 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,45 +3,6 @@
 @ $db = new mysqli('localhost', 'root', '', 'juarvis');
 session_start();
 
-$error=''; // Variable To Store Error Message
-
-if (isset($_POST['submit'])) {
-	
-if (empty($_POST['username']) || empty($_POST['userpass'])) {
-	$error = "Username or Password is invalid";
-}
-else
-{
-	// Define $username and $password
-	$username=$_POST['username'];
-	$password=$_POST['userpass'];
-	
-	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-	$connection = mysql_connect("localhost", "root", "juarvis");
-	
-	// To protect MySQL injection for Security purpose
-	$username = stripslashes($username);
-	$username = htmlentities($username);
-	$password = stripslashes($password);
-	$username = mysql_real_escape_string($username);
-	$password = mysql_real_escape_string($password);
-	
-	// Selecting Database
-	$db = mysql_select_db("users", $connection);
-	
-	// SQL query to fetch information of registerd users and finds user match.
-	$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
-	$rows = mysql_num_rows($query);
-	
-if ($rows == 1) {
-	$_SESSION['username']=$username; // Initializing Session
-	header("location: main.php"); // Redirecting To home page
-} else {
-	$error = "Username or Password is invalid";
-	}
-	mysql_close($connection); // Closing Connection
-	}
-}
 ?> 
 
 <div class="login-form">
