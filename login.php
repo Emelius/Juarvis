c610c6b3565840295b4140e66b5e35a3f91eb68f
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-05 11:21:31 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,62 @@
 
-<!--header start here-->
+<? php 
+	include('header.php');
+?>
+
+<h3>Log in</h3>
+
+<?php
+
+@ $db = new mysqli('localhost', 'root', '', 'juarvis');
+
+if ($db->connect_error) {
+    echo "could not connect: " . $db->connect_error;
+    printf("<br><a href=index.php>Return to home page </a>");
+    exit();
+}
+
+    #the mysqli_real_espace_string function helps us solve the SQL Injection
+    #it adds forward-slashes in front of chars that we can't store in the username/pass
+    #in order to excecute a SQL Injection you need to use a ' (apostrophe)
+    #Basically we want to output something like \' in front, so it is ignored by code and processed as text
+
+if (isset($_POST['username'], $_POST['userpass'])) {
+    #SQL Injection-proof
+    $uname = htmlentities($_POST['username']);
+    $uname = mysqli_real_escape_string($db, $_POST['username']);
+    
+    #without function, so here you can try to implement the SQL injection
+    #various types to do it, either add ' -- to the end of a username, which will comment out
+    #or simply use 
+    #' OR '1'='1' #
+    #$uname = $_POST['username'];
+    
+    #here we hash the password, and we want to have it hashed in the database as well
+    #optimally when you create a user (through code) you simply send a hash
+    #hasing can be done using different methods, MD5, SHA1 etc.
+    
+    $upass = sha1($_POST['userpass']);
+    
+    #just to see what we are selecting, and we can use it to test in phpmyadmin/heidisql
+    
+    //echo "SELECT * FROM users WHERE username = '{$uname}' AND hash = '{$upass}'";
+    
+    $query = ("SELECT * FROM users WHERE username = '{$uname}' "." AND hash = '{$upass}'");
+       
+    
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $stmt->store_result(); 
+    
+    #here we create a new variable 'totalcount' just to check if there's at least
+    #one user with the right combination. If there is, we later on print out "access granted"
+    $totalcount = $stmt->num_rows();
+    
+    
+    
+}
+?>
+
 <div class="login-form">
 			<div class="top-login">
 				<span><img src="img/group.png" alt=""/></span>
