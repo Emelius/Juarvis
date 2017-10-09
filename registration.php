2ce239097c01774104e17419d113a5acd57ddd56
Hanna <hanna_96eri@hotmail.com>
2017-10-09 10:26:03 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,47 +1,33 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Square login form Responsive Widget  Website Template | Home:: w3layouts</title>
-<!-- Custom Theme files -->
-<link href="style.css" rel="stylesheet" type="text/css" media="all"/>
-<!-- Custom Theme files -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<!--Google Fonts-->
-<link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-<!--Google Fonts-->
-</head>
-<body>
-<!--header start here-->
-<div class="login-form">
-			<div class="top-login">
-				<span><img src="img/group.png" alt=""/></span>
-			</div>
-			<h1>Welcome to Juarvis!</h1>
-			<div class="login-top">
-			<form>
-				<div class="login-ic">
-					<i ></i>
-					<input type="text"  value="User name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User name';}"/>
-					<div class="clear"> </div>
+<?php include 'head.php'; ?>
+	<body>
+		<div class="login-form">
+				<div class="top-login">
+					<span><img src="img/group.png" alt=""/></span>
 				</div>
-				<div class="email-ic">
-					<i ></i>
-					<input type="text"  value="email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'email';}"/>
-					<div class="clear"> </div>
+				<h1>Welcome to Juarvis!</h1>
+				<div class="login-top">
+				<form>
+					<div class="login-ic">
+						<i ></i>
+						<input type="text"  value="User name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User name';}"/>
+						<div class="clear"> </div>
+					</div>
+					<div class="email-ic">
+						<i ></i>
+						<input type="text"  value="email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'email';}"/>
+						<div class="clear"> </div>
+					</div>
+					<div class="login-ic">
+						<i class="icon"></i>
+						<input type="password"  value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}"/>
+						<div class="clear"> </div>
+					</div>
+
+					<div class="log-bwn">
+						<input type="submit"  value="Login" >
+					</div>
+					</form>
 				</div>
-				<div class="login-ic">
-					<i class="icon"></i>
-					<input type="password"  value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}"/>
-					<div class="clear"> </div>
-				</div>
-			
-				<div class="log-bwn">
-					<input type="submit"  value="Login" >
-				</div>
-				</form>
-			</div>
-</div>		
-</body>
+		</div>		
+	</body>
 </html>
