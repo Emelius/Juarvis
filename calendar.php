8233a1d2394171ec7dcaee895a6d6b58a6ec5b8a
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-09 11:26:22 +0200

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,8 +1,16 @@
 // insert calendar here, show dates that have tasks //
 
-<?php
-  include ('header.php');
-?>
+<!doctype html>
+<html>
+    <head>
+        <title>JUARVIS</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="main.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
+        <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    </head>
 
 <?php
 
@@ -100,3 +108,5 @@ for ($day = 1; $day <= $day_count; $day++) {
     
  </table>
 </div> 
+
+</html>
