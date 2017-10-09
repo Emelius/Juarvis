ff52a2ad7df3fb3079b40dae920ca721de1e8a68
Hanna <hanna_96eri@hotmail.com>
2017-10-09 10:28:20 +0200

    Update head.php

--- head.php
+++ head.php
@@ -1,4 +1,8 @@
 <?php include 'config.php'; ?>
 <head>
-
+  <title>JUARVIS</title>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" type="text/css" href="main.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
+  <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 </head>
