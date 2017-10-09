85462cdfa4bb60c57944d53b622677bb335e736b
Hanna <hanna_96eri@hotmail.com>
2017-10-09 11:36:19 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,9 +1,36 @@
+<?php include 'header.php'; ?>
+
 <?php
-include 'header.php';
+
+session_start();
+
+$username = $_SESSION["(active user?)"];
+
+echo (current/active username);
+
+echo '<form method="POST" action="registration.php">
+
+<input type="text" value="newusername"/>';
+
+echo (current/active password);
+
+echo 'input type="password" value="newpassword"/>';
+
+echo (current/active name);
+
+echo '<input type="text" value="newname"/>';
+
+echo (current/active email);
+
+echo  '<input type="email" value="newemail"/>
+
+<input type="submit"  value="Save Changesr">';
+
+echo '</form>';
 ?>
-/*/(dependant on session)
-table listing following: to change username, name, change pw, email, colorscheme?
-change button - ability to change - send new info to db /*/
-<?php
-include 'footer.php';
-?>
+
+/*/list following:username, pw, name, email, colorscheme?
+econfirm button - ability to change - send new info to db
+/*/
+
+<?php include 'footer.php'; ?>
