6846379cf3416212577cac10c49b64963a35c559
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-09 11:07:59 +0200

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -60,7 +60,14 @@ for ($day = 1; $day <= $day_count; $day++) {
   //End of the week OR End of the month 
   if($str % 7 == 6 || $day == $day_count) {
     
-    if($day == $day_count)
+    if($day == $day_count){
+      $week .= str_repeat('<td></td>',6-($str % 7));  
+    }
+    
+    $weeks[] = '<tr>'.$week.'</tr>';
+    
+    //Prepare for a new week
+    $week = '';
      
   } 
   
@@ -69,7 +76,11 @@ for ($day = 1; $day <= $day_count; $day++) {
 ?>
 
 <div class="container">
-  <h3><a>back<a/>October 2017<a>forward</a><h3>
+  <h3>
+    <a href="?ym=<?php echo $prev; ?>">&lt;<a/>
+      <?php echo $html_title; ?>
+    <a href="?ym=<?php echo $next; ?>">&gt;</a>
+  <h3>
   <br>
   <table class="table.table-bordered">
     <tr class="week">
@@ -81,50 +92,9 @@ for ($day = 1; $day <= $day_count; $day++) {
       <th>F</th>
       <th>S</th>
     </tr>
-    <tr class="week">
-      <td>1</td>
-      <td>2</td>
-      <td>3</td>
-      <td>4</td>
-      <td>5</td>
-      <td>6</td>
-      <td>7</td>
-    </tr>
-    <tr class="week">
-      <td>8</td>
-      <td class="today">9</td>
-      <td>10</td>
-      <td>11</td>
-      <td>12</td>
-      <td>13</td>
-      <td>14</td>
-    </tr>
-    <tr class="week">
-      <td>15</td>
-      <td>16</td>
-      <td>17</td>
-      <td>18</td>
-      <td>19</td>
-      <td>20</td>
-      <td>21</td>
-    </tr>
-    <tr class="week">
-      <td>22</td>
-      <td>23</td>
-      <td>24</td>
-      <td>25</td>
-      <td>26</td>
-      <td>27</td>
-      <td>28</td>
-    </tr>
-    <tr class="week">
-      <td>29</td>
-      <td>30</td>
-      <td>31</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
+    <?php
+    
+    ?>
+    
  </table>
 </div> 
