9113cd38a8897d255d4141b9bc8f421754cf27cd
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-10 09:37:46 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,13 +1,76 @@
 <?php include 'head.php'; ?>
-	<body>
-		<h1>Welcome to Juarvis!</h1>
-		<p> Fill in your information below to register. </p>
-		<form method='POST' action='registration.php'>
-			<input type="text"  value="username"/>
-			<input type="password" value="password"/>
-			<input type="text" value="name"/>
-			<input type="text"  value="email"/>
-			<input type="submit"  value="Register">
-		</form>	
-	</body>
+	
+<?php
+if (isset($_POST['newbooktitle'])) {
+    // This is the postback so add the book to the database
+    # Get data from form
+    $newusername = trim($_POST['newusername']);
+    $newemail = trim($_POST['newemail']);
+    $newpassword = trim($_POST['newpassword']);
+
+    if (!$newusername || !$newemail || !$newpassword) {
+        printf("You must specify both username, email and a password");
+        printf("<br><a href=registration.php>Try again</a>");
+        exit();
+    }
+
+    $newusername = addslashes($newusername);
+    $newemail = addslashes($newemail);
+    $newpassword = addslashes($newpassword);
+	
+    $newusername = htmlentities($newusername);
+    $newemail = htmlentities($newemail);
+    $newpassword = htmlentities($newpassword);
+
+    # Open the database
+@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+    if ($db->connect_error) {
+        echo "could not connect: " . $db->connect_error;
+        printf("<br><a href=registration.php>Registration failed, try again</a>");
+        exit();
+    }
+
+    // Prepare an insert statement and execute it
+    $stmt = $db->prepare("insert into user values ('', ?, ?, ?, '')");
+    $stmt->bind_param('sss', $newusername, $newemail, $newpassword);
+    $stmt->execute();
+    printf("<br>Account created!");
+    printf("<br><a href=index.php>Login</a>");
+    include("footer.php");
+    exit;
+}
+
+?>
+
+<h3>Welcome to Juarvis</h3>
+
+<form action="registration.php" method="POST">
+    <table bgcolor="#eeeeee" cellpadding="6">
+        <tbody>
+            <tr>
+                <td>Username</td>
+                <td><INPUT type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><INPUT type="text" name="email"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><INPUT type="text" name="password"></td>
+            </tr>
+		<tr>
+                <td>Confirm Password</td>
+                <td><INPUT type="text" name="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><INPUT type="submit" name="submit" value="Register"></td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+</form>
+
 </html>
