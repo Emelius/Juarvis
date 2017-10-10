0d8d418441e1525b9f6d6ca3492fe1677c5a196b
JGranlund <granlund.johannes@gmail.com>
2017-10-10 09:59:22 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,55 +3,50 @@
 <?php
         session_start();
 
-        if (isset($_SESSION['username'])) {
-                $activeusername = $_SESSION['username'];
+        if (isset($_SESSION['login_user'])) {
+                $activeusername = $_SESSION['login_user'];
         }
         else {
-                header("index.php")
+                header("index.php");
         }
 
         @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-		
+
 		if ($db->connect_error) {
 			echo "could not connect: " . $db->connect_error;
 			printf("<br><a href=index.php>Return to home page </a>");
 			exit();
 	}
-	$sql = "SELECT user_id FROM user WHERE username = '$myusername' and password = '$mypassword'";
+	$sql = "SELECT * FROM user WHERE username = 'login_user'";
 	$result = mysqli_query($db,$sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
-	echo $row;
-	
-        $activeemail = //assign from databse email
-	user_id?
-                
-        // check so that new username or email is not already in db?	
+	printf($row);
+
+        //$activeemail =assign from databse email
+
+        // check so that new username or email is not already in db?
 	//if (newusername != any other username in db) {"sorry, the username is taken!}
-		
+
 	//check so that fields are not empty? don't send to db -> if !empty
-	
+
 	//else post -> settingsinsert.php -> insert new info into db
-        
+
         echo
-                '<h2>Username</h2>'
-                ,
-                $activeusername
-                ,
+                "<h2>Username</h2>",
+                $activeusername,
                 '<form method="POST" action="settingsinsert.php">
-		<h3>Change Username</h3>
+		            <h3>Change Username</h3>
                 <input type="text" name="newusername"/>
                 <h2>Change Password</h2>
                 <input type="password" name="newpassword"/>
-                <h2>Email<h2>'
-                ,
-                $activeemail
-                ,
+                <h2>Email</h2>',
+                $activeemail,
                 '<h3>Change Email</h3>
                 <input type="email" name="newemail"/>
                 <input type="submit"  value="Save Changes">
                 </form>';
-	
+
 ?>
 
 <?php include 'footer.php'; ?>
