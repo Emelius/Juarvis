34b1a40b458a038aa8c904c3e402c33561c2fbb1
Hanna <hanna_96eri@hotmail.com>
2017-10-12 14:45:42 +0200

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
         <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
         
-        <link rel="stylesheet" type="text/css" href="main.css"/>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
         
     </head>
 
