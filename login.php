2c08881c26f07a3d04a50545093491aa964ff1a3
JGranlund <granlund.johannes@gmail.com>
2017-10-12 14:33:56 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,43 +1,35 @@
 <?php
     include("config.php");
-    session_start();
-    if (isset($_SESSION['username'])) {
-        header("welcome.php");
+    include("session.php");
+    if (isset($_POST['myusername'], $_POST['mypassword']) && !empty($_POST)) {
+      $myusername =  stripslashes($_POST['myusername']);
+	    $mypassword =  stripslashes($_POST['mypassword']);
+
+	@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+	if ($db->connect_error) {
+		echo "could not connect: " . $db->connect_error;
+		exit();
+	}
+
+	$stmt = $db->prepare("SELECT username, password FROM users WHERE username = ?");
+	$stmt->bind_param('s', $myusername);
+	$stmt->execute();
+
+    $stmt->bind_result($username, $password);
+    while ($stmt->fetch()) {
+      if (sha1($mypassword) == $password){
+	$_SESSION['username'] = $myusername;
+	header("location:welcome.php");
+	exit();
+	}
+       else {
+          $error = "Your Login Name or Password is invalid, please try again.";
+          echo ("<p class=\"error_msg\">$error</p>");
+       }
     }
-    if (isset($_POST["login"])) {
+  }
 
-      if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $db = mysqli_connect($dbserver,$dbuser,$dbpass,$dbname);
-        $myusername = mysqli_real_escape_string($db,$_POST['username']);
-	      $myusername = htmlentities($myusername);
-        $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-	      $mypassword = htmlentities($mypassword);
-        // username and password sent from form, This php script was modified and based on a script from:
-        //https://www.tutorialspoint.com/php/php_mysql_login.htm
-
-        $myusername = mysqli_real_escape_string($db,$_POST['username']);
-        $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-
-        $sql = "SELECT user_id FROM users WHERE username = '$myusername' and password = '$mypassword'";
-        $result = mysqli_query($db,$sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        //$active = $row['active'];
-
-        $count = mysqli_num_rows($result);
-
-        // If result matched $myusername and $mypassword, table row must be 1 row
-
-        if($count == 1) {
-           //session_register("myusername");
-           $_SESSION['login_user'] = $myusername;
-
-           header("location: welcome.php");
-        }else {
-           $error = "Your Login Name or Password is invalid, please try again.";
-           echo ("<p class=\"error_msg\">$error</p>");
-        }
-     }
-   }
 
 ?>
 <html>
@@ -56,8 +48,8 @@
 
                <form action = "" method = "post">
 
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <label>UserName  :</label><input type = "text" name = "myusername" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "mypassword" class = "box" /><br/><br />
                   <input type = "submit" name = "login" value = " Log In "/>
                   <br />
                   <a href="registration.php">Register here...</a>
