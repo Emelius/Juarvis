2e325ad41ee533c13c650bf29311748c1aee404c
Hanna <hanna_96eri@hotmail.com>
2017-10-20 10:30:41 +0200

    Update head.php

--- head.php
+++ head.php
@@ -5,5 +5,4 @@
       <meta charset="utf-8"/>
       <link rel="stylesheet" type="text/css" href="style.css"/>
       <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
-      <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 </head>
