e367b268521ac40d4da880cd170733c7226a7781
Hanna <hanna_96eri@hotmail.com>
2017-10-20 10:27:36 +0200

    Update login.php

--- login.php
+++ login.php
@@ -34,8 +34,6 @@
 
 ?>
 
-   <body bgcolor = "#FFFFFF">
-
       <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
