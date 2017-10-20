36a156b98c36e5dddc6c1568058438f14be703fb
Hanna <hanna_96eri@hotmail.com>
2017-10-20 11:00:15 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,9 +3,9 @@
 	include("session.php");
 	include "head.php";
 
-	if(isset($_SESSION['username'])){
-		header ("location:main.php");
-	}
+	// else {
+	//	header ("location:main.php");
+	//}
 
 	if (isset($_POST['myusername'], $_POST['mypassword']) && !empty($_POST)) {
 	$myusername =  stripslashes($_POST['myusername']);
@@ -22,16 +22,16 @@
 	$stmt->bind_param('s', $myusername);
 	$stmt->execute();
 
-    $stmt->bind_result($username, $password);
-    while ($stmt->fetch()) {
-      if (sha1($mypassword) == $password){
-	$_SESSION['username'] = $myusername;
-	header("location:main.php");
-	exit();
+	$stmt->bind_result($username, $password);
+	while ($stmt->fetch()) {
+	if (sha1($mypassword) == $password){
+		$_SESSION['username'] = $myusername;
+		header("location:main.php");
+		exit();
 	}
-       else {
-          $error = "Your Login Name or Password is invalid, please try again.";
-          echo ("<p class=\"error_msg\">$error</p>");
+	else {
+		$error = "Your Login Name or Password is invalid, please try again.";
+		echo ("<p class=\"error_msg\">$error</p>");
        }
     }
   }
