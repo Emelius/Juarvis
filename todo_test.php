752ebd2d672ff48a911c0642be07fc5f8e0a1e80
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-24 11:15:36 +0200

    zfaef

--- todo_test.php
+++ todo_test.php
@@ -16,6 +16,8 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         echo "id: " . $row["listname"]. "";
+    }else{
+      echo "0 results";
     }
 
   //display all lists and their tasks as a table?
