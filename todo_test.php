e065cbf2d12e3b368fbd02e90484aca5ecdea5e8
Hanna <hanna_96eri@hotmail.com>
2017-10-24 11:07:31 +0200

    Update todo_test.php

--- todo_test.php
+++ todo_test.php
@@ -9,9 +9,19 @@
 	}
 
   //get all lists and their tasks from DB
+	$sql = "SELECT * FROM lists";
+	$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["listname"]. ";
+    }
+
   //display all lists and their tasks as a table?
 
   //create new list
+
   //in this list create new tasks
   //tasks should hold information such as taskname, taskdesc, sdate, edate, rdate, status(meaning if it is completed or not)
 
@@ -20,3 +30,4 @@
   //change task to completed and mark it as grey or other CSS
 
 ?>
+
