15e8c5cd40c64ea75ec648d4c00e584f3029dfa8
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-24 10:42:01 +0200

    test to do

---
+++ todo_test.php
@@ -0,0 +1,16 @@
+<?php
+  include 'config.php';
+
+  //establish db connection
+  //get all lists and their tasks from DB
+  //display all lists and their tasks as a table?
+
+  //create new list
+  //in this list create new tasks
+  //tasks should hold information such as taskname, taskdesc, sdate, edate, rdate, status(meaning if it is completed or not)
+
+  //insert this new lists and tasks to DB
+
+
+
+?>
