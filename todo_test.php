697a200ff9a46ab0021ca2e7c0b8b69ca5f9cee2
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-24 12:47:43 +0200

    lalal

--- todo_test.php
+++ todo_test.php
@@ -28,13 +28,13 @@ else{
   //display all lists and their tasks as a table?
 
 $sql2 = "SELECT taskname FROM tasks where list_id = list_id"; //and user_id = 'login_user'";
-	$result = mysqli_query($db, $sql2);
+	$result2 = mysqli_query($db, $sql2);
 
 $listname = "";
 
-if (mysqli_num_rows($result) > 0) {
+if (mysqli_num_rows($result2) > 0) {
     // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
+    while($row = mysqli_fetch_assoc($result2)) {
         echo "task: " . $row["taskname"]. "<br>";
     }
 }
