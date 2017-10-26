c734332c9b014ead0001964abd79017e7b3c2baa
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-26 15:15:27 +0200

    logout update

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 // You have left jarviz all alone and logged out bye pls thnx
     session_start();
     session_destroy();
-    header("location: ../index.php");
+    header("location:index.php");
     ?>
