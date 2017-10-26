1cbeffcb76ae2f8fb6289e2fe46c3007fb0b3776
JGranlund <granlund.johannes@gmail.com>
2017-10-26 13:24:05 +0200

    ds

--- login.php
+++ login.php
@@ -8,13 +8,8 @@
 		header("location:main.php");
 	}
 
-<<<<<<< HEAD
-	if (isset($_POST['myusername'], $_POST['mypassword']) && !empty($_POST)) {
-	echo "hej";
-=======
-	if (isset($_POST['myusername'], $_POST['mypassword'])) {
+		if (isset($_POST['myusername'], $_POST['mypassword'])) {
 
->>>>>>> 63476ac9309ff8c2888159ceb59b7366fa761e36
 		$myusername =  stripslashes($_POST['myusername']);
 		$mypassword =  stripslashes($_POST['mypassword']);
 
@@ -24,23 +19,16 @@
 		echo "could not connect: " . $db->connect_error;
 		exit();
 	}
-
-<<<<<<< HEAD
-	$stmt = $db->prepare("SELECT username, password FROM users WHERE username = '$myusername'");
-=======
 	echo "SELECT username, password FROM users WHERE username = '$myusername'";
 
 	$stmt = $db->prepare("SELECT username, password FROM users WHERE username = ?");
 	$stmt->bind_param('s', $myusername);
->>>>>>> 63476ac9309ff8c2888159ceb59b7366fa761e36
 	$stmt->execute();
 	$stmt->bind_result($username, $password);
 
 	while ($stmt->fetch()) {
 	if (sha1($mypassword) == $password){
 		$_SESSION['username'] = $myusername;
-
-
 				ob_start();
 				header("location:main.php");
 		ob_flush();
@@ -60,13 +48,10 @@
    <div class="loginDiv">
         <img src="img/juarvis.png" alt="logo" id="logo"/>
 		<h1>Juárvis</h1>
-		<h2>Not your average list making</h2>
-<<<<<<< HEAD
+		<h2>Not your average list making</h2
 		<form class="loginForm" action = "" method = "POST">
-=======
 	   	<h3>Log in below to get started!</h3>
 		<form class="loginForm" action = "" method = "post">
->>>>>>> 63476ac9309ff8c2888159ceb59b7366fa761e36
                   <input type = "text" name = "myusername" placeholder="Username" class = "inputField"/>
                   <br>
                   <input type = "password" name = "mypassword" placeholder="Password" class = "inputField" />
