c55a83668ebb2c7eb80a0611996da8ed0706f1ea
Hanna <hanna_96eri@hotmail.com>
2017-10-31 16:23:01 +0100

    Update main.php

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
     include 'config.php';
     include 'session.php';
     include 'header.php';
-    include 'calendar.php';
     include 'todo.php';
+    include 'calendar.php';
     include 'footer.php'
 ?>
