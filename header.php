a5830c2d6e131d52fbee2d99e11acde8675fbd68
Hanna <hanna_96eri@hotmail.com>
2017-10-31 13:41:30 +0100

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     </head>
     <body>
     	<header>
-            <h1>Juarvis<h1>
+            <h1>Juarvis</h1>
             <!-- display logged in username or name?-->
             <nav id='headermenu'>
                 <ul>
