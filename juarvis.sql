926e688e8ffcd39f6c07378a4c31e61e64d99087
Emelius <32455107+Emelius@users.noreply.github.com>
2017-10-31 11:23:11 +0100

    Add files via upload

--- juarvis.sql
+++ juarvis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 26, 2017 at 03:10 PM
+-- Generation Time: Oct 31, 2017 at 11:23 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -31,9 +31,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `lists` (
   `list_id` int(11) NOT NULL,
   `listname` varchar(255) NOT NULL,
-  `task_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lists`
+--
+
+INSERT INTO `lists` (`list_id`, `listname`, `user_id`) VALUES
+(11, 'My first list', NULL);
 
 -- --------------------------------------------------------
 
@@ -48,7 +54,8 @@ CREATE TABLE `tasks` (
   `sdate` date DEFAULT NULL,
   `edate` date DEFAULT NULL,
   `rdate` date DEFAULT NULL,
-  `status` tinyint(1) DEFAULT NULL
+  `status` tinyint(1) DEFAULT NULL,
+  `list_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -65,6 +72,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
+(10, 'Emelie', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'emelie@emelie.com');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -73,14 +87,14 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `lists`
   ADD PRIMARY KEY (`list_id`),
-  ADD KEY `task_id` (`task_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`task_id`);
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `list_id` (`list_id`);
 
 --
 -- Indexes for table `users`
@@ -96,17 +110,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `lists`
 --
 ALTER TABLE `lists`
-  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Constraints for dumped tables
 --
@@ -115,8 +129,13 @@ ALTER TABLE `users`
 -- Constraints for table `lists`
 --
 ALTER TABLE `lists`
-  ADD CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `lists_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`list_id`) REFERENCES `lists` (`list_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
