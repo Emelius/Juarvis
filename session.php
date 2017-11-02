3684c00ca108431bfe4495f4d2544de08c746bf0
JGranlund <granlund.johannes@gmail.com>
2017-11-02 15:44:37 +0100

    jasmin help

--- session.php
+++ session.php
@@ -3,8 +3,12 @@
    if (session_status() == PHP_SESSION_NONE) {
     session_start();
   }
+  echo $_SESSION['username'];
+  echo $_SESSION['user_id'];
 
-   if(!isset($_SESSION['username'])){
+   if(
+     (!isset($_SESSION['username']) && (!isset($_SESSION['user_id']))))
+   {
      echo "not set";
       //header("location:index.php");
    }
