da352b7c5448ae1b566d6ca0693810d1d72b9405
JGranlund <granlund.johannes@gmail.com>
2017-11-07 11:28:43 +0100

    jg changes

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
         <meta charset="utf-8"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-        <script src="script.js"></script>
         <link rel="stylesheet" type="text/css" href="style.css"/>
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
     </head>
@@ -14,8 +13,8 @@
             <img src='img/juarvis_white.png' alt="logo" id="logo_header"/>
             <nav id='headermenu'>
                 <ul>
-                    <li><a href="main.php">Home</a></li>
-                    <li><a href="settings.php">Settings</a></li>
+                    <li><a class="<?php echo ($current_page == 'main.php')? 'active' :NULL?>" href="main.php">Home</a></li>
+                    <li><a class="<?php echo ($current_page == 'settings.php')? 'active' :NULL?>" href="settings.php">Settings</a></li>
                 </ul>
             </nav>
             <a id='logout' href="logout.php">Log out</a>
