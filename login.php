77e24263698959332678b6101a633fe8dbd09f42
Hanna <hanna_96eri@hotmail.com>
2017-11-11 16:14:38 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,55 +2,54 @@
 	include "config.php";
 	include "head.php";
 
-//checks if a session is started
+	//checks if a session is started, if not redirect to login page
 	if (session_status() == PHP_SESSION_NONE) {
 	    session_start();
 	}
 	if(isset($_SESSION['username'])){
 		header("location:main.php");
 	}
-//checks if form has send any data
+	//checks if form has send any data
 	if (isset($_POST['myusername'], $_POST['mypassword'])) {
+		
 		//removes all the slashes to convert the text to a one line string.
 		$myusername =  stripslashes($_POST['myusername']);
 		$mypassword =  stripslashes($_POST['mypassword']);
 		//protects the forms with html entities, and protects against html injection
 		$myusername = htmlentities($_POST['myusername']);
 		$mypassword = htmlentities($_POST['mypassword']);
+		//adds slashes before certain variables stopping harmful code form entering the db.
+		$myusername = mysqli_real_escape_string($db, $myusername);
+		$mypassword = mysqli_real_escape_string($db, $mypassword);
+		//returns a string with added backslashes infront of predefined characters ',",\,NULL
+		$myusername = addslashes($myusername);
+		$mypassword = addslashes($mypassword);
+		
+		//db connection
+		@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+		if ($db->connect_error) {
+			echo "could not connect: " . $db->connect_error;
+			exit();
+		}
 
-	@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-	//adds slashes before certain variables stopping harmful code form entering the db.
-	$myusername = mysqli_real_escape_string($db, $myusername);
-	$mypassword = mysqli_real_escape_string($db, $mypassword);
-	//returns a string with added backslashes infront of predefined characters ',",\,NULL
-	$myusername = addslashes($myusername);
-	$mypassword = addslashes($mypassword);
+		//fetches the user_id username and password from the db where username is euqal to the variable containing what the user inserted inot the form.
+		$stmt = $db->prepare("SELECT user_id, username, password FROM users WHERE username = ?");
+		$stmt->bind_param('s', $myusername);
+		$stmt->bind_result($userid,$username, $password);
+		$stmt->execute();
 
-
-	if ($db->connect_error) {
-		echo "could not connect: " . $db->connect_error;
-		exit();
+		//fetches the stmt and checks if the hashed password the user inserted into the form is the same as the one in the db
+		while ($stmt->fetch()) {
+		if (sha1($mypassword) == $password){
+			//adds data from username and userid variable to the session.
+			$_SESSION['username'] = $username;
+			$_SESSION['user_id'] = $userid;
+			ob_start();
+			header("location:main.php");
+			ob_flush();
+			exit();
 	}
-
-
-//fetches the user_id username and password from the db where username is euqal to the variable containing wha the user inserted inot the form.
-	$stmt = $db->prepare("SELECT user_id, username, password FROM users WHERE username = ?");
-	$stmt->bind_param('s', $myusername);
-	$stmt->bind_result($userid,$username, $password);
-	$stmt->execute();
-
-//fetches the stmt and checks if the hashed password the user inserted into the form is the same as the one in the db
-	while ($stmt->fetch()) {
-	if (sha1($mypassword) == $password){
-		//adds data from username and userid variable to the session.
-		$_SESSION['username'] = $username;
-		$_SESSION['user_id'] = $userid;
-		ob_start();
-		header("location:main.php");
-		ob_flush();
-		exit();
-	}
-	//otherwise tells the user to retry with a different username or password.
+	//else tells the user to retry with a different username or password.
 	else {
 		$error = "Your Username or Password is invalid, please try again.";
 		echo ("<p class=\"error_msg\">$error</p>");
